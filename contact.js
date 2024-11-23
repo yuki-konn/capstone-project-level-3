f9bef273c89b709bb0aee35f0bdad7ebf7bb7b51
yuki-konn <gakusei.saru@gmail.com>
2024-11-23 01:35:32 -0600

    added comment

--- contact.js
+++ contact.js
@@ -128,6 +128,7 @@ function getServerResponse(resolve) {
     const response = {
       message: "<b style='color: green'>SUCCESSFULLY PROCESSED</b>",
     };
+    // Server stringifies a object and replies with a string.
     const resolveValue = JSON.stringify(response);
     resolve(resolveValue);
   }

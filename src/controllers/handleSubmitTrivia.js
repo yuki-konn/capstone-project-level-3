fe5bc5a8940eef1adf58093aeb4816ea36222749
yuki-konn <gakusei.saru@gmail.com>
2025-01-23 13:16:08 -0600

    deleted code and contact.html

--- src/controllers/handleSubmitTrivia.js
+++ src/controllers/handleSubmitTrivia.js
@@ -6,21 +6,4 @@ window.handleSubmitTrivia = handleSubmitTrivia;
 function handleSubmitTrivia(event = new HTMLFormElement()) {
   debugger;
   event.preventDefault();
-  // const form = event.target;
-  // const choiceA = form[0];
-  // const choiceB = form[1];
-  // const choiceC = form[2];
-  // const choiceD = form[3];
-  // const value = {
-  //   choiceA.value
-  // };
-  // if (choiceA.checked) {
-  //   output2(`<span class="text-danger">Incorrect answer</span>`);
-  // } else if (choiceB.checked) {
-  //   output2(`<span class="text-danger">Incorrect answer</span>`);
-  // } else if (choiceC.checked) {
-  //   output2(`<span class="text-success">Correct answer</span>`);
-  // } else if (choiceD.checked) {
-  //   output2(`<span class="text-danger">Incorrect answer</span>`);
-  // }
 }

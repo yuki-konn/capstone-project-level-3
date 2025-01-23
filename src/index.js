a134e86de4de31ce90457ccb1658cc66fa83e3fc
yuki-konn <gakusei.saru@gmail.com>
2025-01-23 06:18:59 -0600

    added BrowserRouter and created branch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const root = createRoot(window.bodyTag);
 root.render(
-  <>
-    <main></main>
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<Placeholder />} />
+      <Route path="/shop" element={<Placeholder />} />
+      <Route path="/about" element={<Placeholder />} />
+      <Route path="/contact" element={<Placeholder />} />
+    </Routes>
+  </BrowserRouter>
 );
